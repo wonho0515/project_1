19f39de6e9fbdcac69587d10dc56b9b301049a92
wonho0515 <november5443@naver.com>
2023-03-16 14:38:40 +0900

    adding files

--- src/Echo.java
+++ src/Echo.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Echo {
     public static void main(String[] args){
-        String air, cloud;
+        String air, cloud; //air와 cloud변수를 문자열로 선언
         Scanner sc = new Scanner (System.in);
 //문자열을 입력받고 next와 nextLine을 활용하여 문자열을 출력
-        air = sc.nextLine ();
-        cloud = sc.next();
+        System.out.println("오늘의 날씨 :");
+        air = sc.nextLine (); //입력 받은 오늘의 날씨 정보를 air에 저장
+        System.out.println("구름 높이 :");
+        cloud = sc.next(); //입력 받은 구름 높이에 관한 정보를 cloud에 저장
         System.out.println("Today weather = " +air);
         System.out.println("cloud height = " +cloud);
     }
--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class TemperatureConverter {
     public static void main (String[] args){
-        int f_tem;
-        double c_tem;
+        int f_tem; //화씨온도를 정수형 표현
+        double c_tem; //섭씨온도를 실수형 표현
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //정수를 입력받기위해 Scanner 클래스 활용
         System.out.println("화씨 온도를 입력하세요 :");
-        f_tem = sc.nextInt();
+        f_tem = sc.nextInt(); //화씨 온도를 입력받고 f_tem에 저장
 
         c_tem = (double)5/9*(f_tem-32); // 5/9가 0이 나오기때문에 double형으로 형변환
-        System.out.printf("섭씨 온도 : %.3f", c_tem); // double형을 출력하기위해 printf사용, c_tem은 소수점 3자리까지 표현
+        System.out.printf("섭씨 온도 : %.3f",c_tem); // c_tem을 소수점 3자리까지만 표현해주기 위해 서식문자를 활용해준다. 그렇기 때문에 printf를 사용한다
     }
 }
