cf69a915833532c63652db222e54e7e12a50307d
wonho0515 <november5443@naver.com>
2023-03-19 20:12:12 +0900

    adding project files

---
+++ src/Myproject.java
@@ -0,0 +1,15 @@
+public class Myproject {
+    //목표 : 관리자가 공공자전거 대여현황에 대한 정보를 확인하고
+    // 공공자전거를 추가 또는 삭제 가능하고 대여한 학생에 대한 정보를 추가 및 삭제 할 수 있는 프로그램
+    public static void main(String [] args){
+        System.out.println("Bicycle rental status"); //자전거 대여현황(잔여 개수, 대여 정보)
+        System.out.println("Add bike"); //자전거 추가
+        System.out.println("Delete bike");// 자전거 삭제
+        System.out.println("Bike rental"); // 자전거 대여(대여 학생에 대한 정보)
+        System.out.println("Bike return"); // 자전거 반납
+        System.out.println("Exit"); //나가기
+
+    }
+
+
+}
