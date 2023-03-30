59bbb0cb0a9178b41a77ccd8256e522026382b49
wonho0515 <november5443@naver.com>
2023-03-30 14:21:02 +0900

    adding project files

---
+++ src/sqare.java
@@ -0,0 +1,11 @@
+public class sqare {
+    public static void main(String[] args){
+        int[] values={12,24,-23,47};
+        int size = values.length;
+
+        for(int i=0;i<4;i++){
+            System.out.println(values[i]);
+        }
+        System.out.println(size);
+    }
+}
