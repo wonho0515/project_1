c8ff5e7d2011eb0aeb72e109ec9bd0e0f8950ade
wonho0515 <november5443@naver.com>
2023-05-21 02:21:46 +0900

    adding project files

--- src/TeamprojectFurniture/PlasticFurniture.java
+++ src/TeamprojectFurniture/PlasticFurniture.java
@@ -2,7 +2,7 @@ package TeamprojectFurniture;
 
 import java.util.Scanner;
 
-public class PlasticFurniture extends Furniture  {
+public class PlasticFurniture extends MiddleFurniture  {
 
     public PlasticFurniture(FurnitureKind kind){
         super(kind);
@@ -13,27 +13,7 @@ public class PlasticFurniture extends Furniture  {
         setFurnitureName(input);
         setFurnitureBrand(input);
         setFurnitureCost(input);
-
-        char answer='x';
-        while(answer!='y'&&answer!='Y'&&answer!='n'&&answer!='N'){
-            System.out.println("Do you any caution? (Y/N)");
-            answer=input.next().charAt(0);
-            if(answer=='y'||answer=='Y'){
-                setFurnitureCaution(input);
-                break;
-            }
-            else if(answer=='n'|| answer=='N'){
-                this.setCaution("");
-                break;
-            }
-            else{
-
-            }
-        }
-    }
-    public void printInfo(){
-        String skind = getKindString();
-        System.out.println("kind: "+skind+", id:"+id+", funiture: "+funiture+", brand: "+brand+", price: "+price+", caution: "+caution);
+        setFurnitureCautionwithYN(input);
     }
 }
 
--- src/TeamprojectFurniture/WoodFurniture.java
+++ src/TeamprojectFurniture/WoodFurniture.java
@@ -2,7 +2,7 @@ package TeamprojectFurniture;
 
 import java.util.Scanner;
 
-public class WoodFurniture extends Furniture {
+public class WoodFurniture extends MiddleFurniture{
 
     public WoodFurniture(FurnitureKind kind){
         super(kind);
@@ -14,9 +14,4 @@ public class WoodFurniture extends Furniture {
         setFurnitureCost(input);
         setFurnitureCaution(input);
     }
-    public void printInfo(){
-        String skind = getKindString();
-        System.out.println("kind: "+skind+", id:"+id+", funiture: "+funiture+", brand: "+brand+", price: "+price+", caution: "+caution);
-    }
-
 }
